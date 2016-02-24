fce6ff3fc17d39e78c433ffb2bd07e406fef4c18
akratsios <akratsios18@gmail.com>
2016-02-24 01:10:12 -0500

    idk

---
+++ hw03/animate.js
@@ -0,0 +1,81 @@
+/*Andrew Kratsios
+SoftDev2 pd 3
+HW#03 -- Are We Ever Going to Start the Movie?
+2016-02-23*/
+
+var c = document.getElementById("playground");
+var ctx = c.getContext("2d");
+var r = document.getElementById("clear");
+var go = document.getElementById("start");
+var d = document.getElementById("devede");
+var requestID;
+
+var dir = 0;
+var radius = 0;
+var xcor = 100;
+var ycor = 400;
+var xdir = 1;
+var ydir = 1;
+var logo = new Image();
+logo.src = "logo_dvd.jpg";
+
+function setup(){
+    ctx.strokeRect(0,0,538,538); 
+}
+
+
+var drawDot = function(){
+ 
+
+    ctx.clearRect(0,0,538,538);
+    ctx.strokeRect(0,0,538,538); 
+    ctx.beginPath();
+    ctx.arc( 538/2 , 538/2 , radius , 0 , 2*Math.PI );
+    ctx.stroke();
+    ctx.fillStyle = "green";
+    ctx.fill();
+
+    if(dir == 0)
+        radius+=3;
+    else
+        radius-=3;
+    if(radius >= 538/2)
+        dir = 1;
+    if(radius <= 0)
+        dir = 0;
+
+    requestID = window.requestAnimationFrame( drawDot );
+} 
+
+var stopIt = function(){
+    window.cancelAnimationFrame(requestID);
+}
+
+var dvd = function(){
+    ctx.clearRect(0,0,538,538);
+    ctx.strokRect(0,0,538,538);
+
+    ctx.drawImage(logo,xcor,ycor,75,50);
+    xcor = xcor + (2 * xdir);
+    ycor = ycor + (2 * ydir);
+    if (xcor == (538-70) || xcor == 0){
+        xdir = xdir * -1;
+    }
+    if (y == (538-50) || y == 0){
+        ydir = ydir * -1;
+    }
+    requestId = window.requestAnimationFrame(dvd);
+}
+
+var empty = function empty(event){
+    event.preventDefault();
+    ctx.clearRect(0,0,538,538);
+    ctx.strokeRect(0,0,538,538); 
+    radius = 0;
+}
+
+setup();
+c.addEventListener("click", drawDot);
+r.addEventListener("click", stopIt);
+go.addEventListener("click", drawDot);
+d.addEventListener("click", dvd);
